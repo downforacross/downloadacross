dabd1e9c49b20a8f50ddcb2706f2b7fc5fe24854
stevenhao <stevenhao@gmail.com>
2017-12-14 21:46:12 -0500

    rename jonesin

--- popup.js
+++ popup.js
@@ -9,7 +9,7 @@ var loaders = {
   'WSJ': WSJLoader,
   'New York Times': NYTimesLoader,
   'NYT Mini': NYTimesMiniLoader,
-  "Jonesin' Puzzles": JzLoader,
+  "Jonesin'": JzLoader,
 };
 
 var allSources = Object.keys(loaders);
