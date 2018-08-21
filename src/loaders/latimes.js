ae8e9a217220d348a7e2a717cfd493c0c108a262
stevenhao <stevenhao@gmail.com>
2018-08-21 14:33:45 -0700

    lat

--- src/loaders/latimes.js
+++ src/loaders/latimes.js
@@ -4,33 +4,32 @@
 
 (function(){
 
-function loadLA(url, date, callback) {
-  fetch(url,
-    function success(response) {
+function loadLA(url, date) {
+  return load(url)
+    .then(function(response) {
       var puzzle = parseCCXML(response, date);
       if (!puzzle) {
-        callback();
-      } else {
-        var filename = 'lat' + date.str + date.dayOfWeekStr + '.puz';
-        puzzle.filename = filename;
-        var ratingUrl = `http://crosswordfiend.com/ratings_count_json.php?puzz=${date.strHyphens}-la`;
-        getCFRating(ratingUrl, function(rating) {
+        throw new Error('failed to parse ccxml');
+      }
+      var filename = 'lat' + date.str + date.dayOfWeekStr + '.puz';
+      puzzle.filename = filename;
+      var ratingUrl = `http://crosswordfiend.com/ratings_count_json.php?puzz=${date.strHyphens}-la`;
+      return getCFRating(ratingUrl)
+        .then(function(rating) {
           if (rating) {
             puzzle.rating = rating;
             var link = `http://crosswordfiend.com/${date.yesterday().strSlashes}/${date.strHyphens}/#la`;
             puzzle.rating.link = link;
           }
-          callback(puzzle);
+          return puzzle;
         });
-      }
-    },
-  );
+    });
 }
 
 window.LATimesLoader = {
-  load: function(date, callback) {
+  load: function(date) {
     var url = `http://cdn.games.arkadiumhosted.com/latimes/assets/DailyCrossword/la${date.str}.xml`;
-    loadLA(url, date, callback);
+    return loadLA(url, date);
   },
 };
 }());
