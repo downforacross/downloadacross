7102425970a47adcf19241d3ed0ea6c65c0f9cdd
stevenhao <stevenhao@gmail.com>
2018-08-21 14:41:09 -0700

    ark mini

--- src/loaders/arkadiummini.js
+++ src/loaders/arkadiummini.js
@@ -4,26 +4,24 @@
 (function(){
 
 function loadArkadiumMini(url, date, callback) {
-  fetch(url,
-    function success(response) {
+  return load(url)
+    .then(function(response) {
       var puzzle = parseCCXML(response, date);
       if (!puzzle) {
-        callback();
-      } else {
-        var filename = 'ark' + date.str + date.dayOfWeekStr + '.puz';
-        puzzle.filename = filename;
-        var title = 'Arkadium Mini ' + date.date.toLocaleDateString("en-US", { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', timeZone: "UTC" });
-        puzzle.meta.title = title;
-        callback(puzzle);
+        throw new Error('failed to parse CCXML');
       }
-    },
-  );
+      var filename = 'ark' + date.str + date.dayOfWeekStr + '.puz';
+      puzzle.filename = filename;
+      var title = 'Arkadium Mini ' + date.date.toLocaleDateString("en-US", { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', timeZone: "UTC" });
+      puzzle.meta.title = title;
+      return puzzle;
+    });
 }
 
 window.ArkadiumMiniLoader = {
-  load: function(date, callback) {
+  load: function(date) {
     var url = `http://ams.arkadiumhosted.com/assets/gamesfeed/minicrossword/puzzle_mini_${date.str}.xml`;
-    loadArkadiumMini(url, date, callback);
+    return loadArkadiumMini(url, date );
   },
 };
 }());
