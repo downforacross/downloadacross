8657be7665149a583faac4faaff8e7c8deb81183
stevenhao <steven@scaleapi.com>
2020-04-14 11:12:17 -0700

    use application/octet-stream in download to allow .puz extension, fixes #11

--- src/download.js
+++ src/download.js
@@ -1,23 +1,12 @@
-var makeAndClickDownloadURL = function(data, fileName) {
-  var a;
-  a = document.createElement('a');
-  a.href = data;
-  a.download = fileName;
-  document.body.appendChild(a);
-  a.style = 'display: none';
-  a.click();
-  a.remove();
-};
-
-var downloadBlob = function(data, filename) {
+var downloadBlob = function (data, filename) {
   var blob, url;
-  blob = new Blob([data]);
+  blob = new Blob([data], { type: "application/octet-stream;charset=UTF-8" });
   url = window.URL.createObjectURL(blob);
   chrome.downloads.download({
     url,
     filename,
-  })
-  setTimeout(function() {
+  });
+  setTimeout(function () {
     return window.URL.revokeObjectURL(url);
   }, 1000);
 };
