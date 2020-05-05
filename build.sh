f0e5a903b284c7b152cb7d17f3dd8dff51baf446
stevenhao <stevenhao@gmail.com>
2020-05-05 09:41:08 -0700

    zsh build.sh + extension version in file name

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
+#!/bin/zsh
 rm -rf build
 mkdir build
 cp -r src/* build
 cp manifest.json build
 cp node_modules/puzjs/puz.js build
-zip -r extension.zip build
+version=$(jq .version manifest.json)
+version=${version:1:-1}
+zip -r extension_${version}.zip build
